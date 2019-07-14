39706b8bc1e0081d18ae687048b548c2e40f7edd
Jamshid93 <hamzayevjf@gmail.com>
2019-07-14 22:50:58 +0500

    This is words nubers output step by step

---
+++ forstring.py
@@ -0,0 +1,7 @@
+word="Tashkent capital of Uzbekistan"
+#print(word[0]) output latter "T"
+# this print  code 0=T, 1=a, 2=s,......else -1=n, -2=a, -3=t, ......
+# print(word[-1]) # output latter "n"
+# print(word[1:-1]) #output words "ashkent capital of Uzbekista"
+# print(word[1:]) #output wors "ashkent capital of Uzbekistan"
+print (word[1:4]) #output latters " ash "
